3ad22f06dc66c556529627408ad191c841cb3953
juun9714 <lovekwon200@gmail.com>
2021-06-25 18:47:40 +0900

    generic view

--- polls/urls.py
+++ polls/urls.py
@@ -3,13 +3,24 @@ from . import views
 
 app_name='polls'
 
+# urlpatterns = [
+#     # ex: /polls/
+#     path('', views.index, name='index'),
+#     # ex: /polls/5/ <- 상세페이지 
+#     path('<int:question_id>/', views.detail, name='detail'),
+#     # ex: /polls/5/results/
+#     path('<int:question_id>/results/', views.results, name='results'),
+#     # ex: /polls/5/vote/
+#     path('<int:question_id>/vote/', views.vote, name='vote'),
+# ]
+
+
+#Generic view
 urlpatterns = [
-    # ex: /polls/
-    path('', views.index, name='index'),
-    # ex: /polls/5/ <- 상세페이지 
-    path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
+
+#pk값을 넘겨주겠거니, 하고 내부적으로 이미 구현이 다 되어 있음
--- polls/views.py
+++ polls/views.py
@@ -4,20 +4,43 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.template import loader
 from .models import Question, Choice
+from django.views import generic
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
-    #render : loader와 HttpResponse를 import하지 않고, 한번에 합쳐서 처리할 수 있는 !
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {'latest_question_list': latest_question_list}
+#     return render(request, 'polls/index.html', context)
+#     #render : loader와 HttpResponse를 import하지 않고, 한번에 합쳐서 처리할 수 있는 !
 
-def detail(request, question_id):
-    question=get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html",{'question':question})
+# def detail(request, question_id):
+#     question=get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/detail.html",{'question':question})
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
+
+#Generic view
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+    #context변수의 이름이 model명이랑 다를 땐, context_object_name에 context변수 이름을 명시해준다.
+
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    #model을 명시해주면, 해당 모델 이름을 자동으로 context변수로 template에 넘겨준다.
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
